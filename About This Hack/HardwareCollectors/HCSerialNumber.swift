11831b4b13e9858faef7abf67699fc6c4136d1dd
matxpa <70573409+matxpa@users.noreply.github.com>
2023-09-28 19:16:41 +0200

    Update HCSerialNumber.swift Glob. Var & toolTip

--- About This Hack/HardwareCollectors/HCSerialNumber.swift
+++ About This Hack/HardwareCollectors/HCSerialNumber.swift
@@ -3,6 +3,13 @@ import Foundation
 class HCSerialNumber {
     
     static func getSerialNumber() -> String {
-        return run("awk '/Serial/ {print $4}' ~/.ath/hw.txt")
+        return run("awk '/Serial/ {print $4}' " + initGlobVar.hwFilePath)
+    }
+    
+    static func getHardWareInfo() -> String {
+        return run("cat " + initGlobVar.hwFilePath + " | egrep \"[System Firmware |OS Loader |SMC ]Version|Apple ROM Info:|Board-ID :|Hardware UUID:|Provisioning UDID:\" | sed -e 's/^ *//g' -e 's/^/      /g'")
+
+// Uncredible the code below returns nothing, the same code above with a "cat file" before "egrep" returns the expected informations
+//        return run("egrep \"System Firmware |OS Loader |SMC ]Version|Apple ROM Info:|Board-ID :|Hardware UUID:|Provisioning UDID:\" " + initGlobVar.hwFilePath + " | sed -e 's?^ *??00g' -e 's?^?      ?g'")
     }
 }
