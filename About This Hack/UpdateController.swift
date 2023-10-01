afd5d97ee1201ad33a9348a8b6758c665ef44135
matxpa <70573409+matxpa@users.noreply.github.com>
2023-10-01 19:17:32 +0200

    Update UpdateController.swift Upgrade modifcations

--- About This Hack/UpdateController.swift
+++ About This Hack/UpdateController.swift
@@ -31,33 +31,31 @@ class UpdateController {
         print("Starting Update...")
         print("Starting Download...")
         notify(title: "Starting Download", informativeText: "This may take awhile...")
-        _ = run("curl -L " + initGlobVar.lastAthreleaseURL + run("tr -d '[:space:]' <  " + initGlobVar.athtargetversionfile) + " " + initGlobVar.newAthziprelease + " -o " + initGlobVar.athDirectory + initGlobVar.newAthreleasezip)
+        _ = run("curl -L " + initGlobVar.lastAthreleaseURL + run("tr -d '[:space:]' <  " + initGlobVar.athtargetversionfile) +  initGlobVar.newAthziprelease + " -o " + initGlobVar.newAthreleasezip)
         print("Killing Old App...")
         notify(title: "Replacing Apps", informativeText: "Deleting the old version and replacing it with the new version")
         // Thanks for the code, Ben216k
-        let rm = run("rm -rf " + initGlobVar.thisAppliLocation)
+        let rm = run("rm -rf \"" + initGlobVar.thisAppliLocation + "\"")
         if rm.contains("No") {
             notify(title: "Failed to delete the old copy of About This Hack.app", informativeText: "Please make sure it is in the Applications folder!!!")
             return
         }
-        _ = run("[[ ! -d '" + initGlobVar.thisAppliLocation + "' ]]")
+        _ = run("[[ ! -d \"" + initGlobVar.thisAppliLocation + "\" ]]")
         print("Unzipping Archive...")
         notify(title: "Unzipping Archive", informativeText: "")
         do {
-            _ = try Zip.unzipFile(URL(string: " " + initGlobVar.alexanderskulaURL + initGlobVar.athfilesDirectory + initGlobVar.newAthreleasezip)!, destination: URL(string: " " + initGlobVar.alexanderskulaURL)!, overwrite: true, password: "")
+            _ = try Zip.unzipFile(URL(string: " " + initGlobVar.newAthreleasezip)!, destination: URL(string: " " + initGlobVar.athDirectory)!, overwrite: true, password: "")
         } catch {
             print(error)
         }
         
-        
-        print("Copying New Version...")
+         print("Copying New Version...")
         notify(title: "Copying New Version", informativeText: "Almost there!")
-        _ = run("cp -rf " + NSHomeDirectory() + initGlobVar.thisAppliname + " " + initGlobVar.allAppliLocation)
+        _ = run("cp -rf " + initGlobVar.athDirectory + "\"" + initGlobVar.thisAppliname + "\" " + initGlobVar.allAppliLocation)
 
         notify(title: "Update Complete!", informativeText: "Launching New Version...")
-        _ = run("open " + initGlobVar.allAppliLocation + initGlobVar.thisAppliname)
-        exit(0)
-    }
+        _ = run("open " + initGlobVar.allAppliLocation + "\"" + initGlobVar.thisAppliname + "\"")
+        exit(0)    }
     
     static func notify(title: String, informativeText: String) -> Void {
         let notification = NSUserNotification()
